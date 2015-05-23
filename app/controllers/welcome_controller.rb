cc88a616b0b9cc020a713d010eb99f82c48632a8
Brown0101 <dwayne@dontasweb.com>
2015-05-23 12:23:16 -0500

    built out navigation and added welcome controller and set root

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def feature
+  end
+
+  def fact
+  end
+
+  def pricing
+  end
+
+  def login
+  end
+end
